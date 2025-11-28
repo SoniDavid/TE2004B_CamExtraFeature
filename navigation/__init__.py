0d12f99ceb01cdb9eefb424ca61761ee0697f8bb
SoniDavid <sonidavid46@gmail.com>
2025-11-28 12:15:13 -0600

    Changed implementation, addedHSV camera filter follower

---
+++ navigation/__init__.py
@@ -0,0 +1,7 @@
+"""
+Navigation follower module for TE2004B Robot Tractor
+Contains different navigation sstrategies:
+- ArUco marker detection and distance estimation
+- Color-based object tracking 
+- (That's all for now)
+"""
